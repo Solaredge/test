38b6a04cc09f91405e11e20309b205bd67bc6fed
Peot <Solaredge@hotmail.co.uk>
2008-10-15 13:57:37 -0500

    yeeha

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,18 @@
+class PublicController < ApplicationController
+  
+  layout "application"
+  
+  def index
+
+    
+    @message = "Welcome to the best site in the world, other than the Rainbow Bars."
+    render :layout => "public"
+  end
+
+  def faq
+  end
+
+  def about
+  end
+
+end
